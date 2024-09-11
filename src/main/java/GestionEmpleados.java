e7602cc851a64c4825ff7d4dc682e966fff930c3
pablo <pablo@localhost>
2024-09-10 21:23:17 -0600

    Se finalizo el proyecto, listo para prueba

--- src/main/java/GestionEmpleados.java
+++ src/main/java/GestionEmpleados.java
@@ -18,13 +18,47 @@ public class GestionEmpleados {
 
     public void cargaDatosDomina() {
         for (Empleado empleado : empleados) {
-            if (empleado instanceof TrabajadorXComision) {
-                
-            } else if (empleado instanceof TrabajadorXHora) {
-                
-            } else if (empleado instanceof TrabajadorXPieza) {
-                
+            switch (empleado) {
+                case TrabajadorXComision trabajadorXComision -> {
+                    System.out.println(empleado.getNombres() + " " + empleado.getApellidoPaterno() + " " + empleado.getApellidoPaterno() + "\tTRABAJADOR POR COMISION");
+                    do {
+                        try {
+                            System.out.print("Introduzca la cantidad vendida: $");
+                            trabajadorXComision.setCantidad(Integer.parseInt(sc.nextLine()));
+                            break;
+                        } catch (NumberFormatException e) {
+                            System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+                        }
+                    } while (true);
+                }
+                case TrabajadorXHora trabajadorXHora -> {
+                    System.out.println(empleado.getNombres() + " " + empleado.getApellidoPaterno() + " " + empleado.getApellidoPaterno() + "\tTRABAJADOR POR HORA");
+                    do {
+                        try {
+                            System.out.print("Introduzca el numero de horas trabajadas: ");
+                            trabajadorXHora.setHorasTrabajadas(Integer.parseInt(sc.nextLine()));
+                            break;
+                        } catch (NumberFormatException e) {
+                            System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+                        }
+                    } while (true);
+                }
+                case TrabajadorXPieza trabajadorXPieza -> {
+                    System.out.println(empleado.getNombres() + " " + empleado.getApellidoPaterno() + " " + empleado.getApellidoPaterno() + "\tTRABAJADOR POR PIEZA");
+                    do {
+                        try {
+                            System.out.print("Introduzca la de piezas producidas: ");
+                            trabajadorXPieza.setCantidad(Integer.parseInt(sc.nextLine()));
+                            break;
+                        } catch (NumberFormatException e) {
+                            System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+                        }
+                    } while (true);
+                }
+                default -> {
+                }
             }
+            System.out.println("");
         }
     }
 
@@ -249,7 +283,98 @@ public class GestionEmpleados {
         }
     }
 
-//    public abstract void generarNomina(String periodo);
+    public void generarNomina() {
+        int dia = 1, mes = 1, anio = 1, contador = 1;
+        System.out.println("Introduzca el periodo de pago");
+        System.out.println("\n-----D E S D E-----");
+        do {
+            try {
+                System.out.print("Ingrese el dia: ");
+                dia = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+            }
+        } while (true);
+        do {
+            try {
+                System.out.print("Ingrese el mes: ");
+                mes = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+            }
+        } while (true);
+        do {
+            try {
+                System.out.print("Ingrese el anio: ");
+                anio = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+            }
+        } while (true);
+        Fecha inicio = new Fecha(dia, mes, anio);
+        System.out.println("\n-----H A S T A-----");
+        do {
+            try {
+                System.out.print("Ingrese el dia: ");
+                dia = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+            }
+        } while (true);
+        do {
+            try {
+                System.out.print("Ingrese el mes: ");
+                mes = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+            }
+        } while (true);
+        do {
+            try {
+                System.out.print("Ingrese el anio: ");
+                anio = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+            }
+        } while (true);
+        Fecha fin = new Fecha(dia, mes, anio);
+        System.out.println("\n\n--------------------P E R I O D O   D E   P A G O----------" 
+                           + "\n                  ("+ inicio + "    al    " + fin+")");
+       
+        System.out.printf("\n%-5s%-20s%-30s%-10s", "NO.", "RFC", "NOMBRE", "TIPO DE EMPLEADO");
+        System.out.printf("\n%-5s%-20s%-15s%-15s%-10s", " ", "SUELDO BASE", "COMISION/", "CANTIDAD/" , "SUELDO");
+        System.out.printf("\n%-5s%-20s%-15s%-15s%-10s\n", " ", " ", "SUELDOXHORA", "HRS" , " ");
+        for (Empleado empleado : empleados) {
+            switch (empleado) {
+                case Jefe jefe -> {
+                    System.out.printf("\n%-5s%-20s%-30s%-10s", contador, empleado.getRfc(), empleado.getNombres() + " " + empleado.getApellidoPaterno() + " " + empleado.getApellidoPaterno(), "Jefe");
+                    System.out.printf("\n%-5s%-20s%-30s%-10s\n\n", " ", jefe.getSueldoSemanal(), " ", empleado.sueldo());
+                }
+                case TrabajadorXComision trabajadorXComision -> {
+                    System.out.printf("\n%-5s%-20s%-30s%-10s", contador, empleado.getRfc(), empleado.getNombres() + " " + empleado.getApellidoPaterno() + " " + empleado.getApellidoPaterno(), "Empleado por comision");
+                    System.out.printf("\n%-5s%-20s%-15s%-15s%-10s\n\n", " ", trabajadorXComision.getSalarioBase(), trabajadorXComision.getComision(),trabajadorXComision.getCantidad(), empleado.sueldo());
+                }
+                case TrabajadorXHora trabajadorXHora -> {
+                    System.out.printf("\n%-5s%-20s%-30s%-10s", contador, empleado.getRfc(), empleado.getNombres() + " " + empleado.getApellidoPaterno() + " " + empleado.getApellidoPaterno(), "Empleado por hora");
+                    System.out.printf("\n%-5s%-20s%-15s%-15s%-10s\n\n", " ", "", trabajadorXHora.getPagoXHora(),trabajadorXHora.getHorasTrabajadas(), empleado.sueldo());
+                }
+                case TrabajadorXPieza trabajadorXPieza -> {
+                    System.out.printf("\n%-5s%-20s%-30s%-10s", contador, empleado.getRfc(), empleado.getNombres() + " " + empleado.getApellidoPaterno() + " " + empleado.getApellidoPaterno(), "Empleado por pieza");
+                    System.out.printf("\n%-5s%-20s%-15s%-15s%-10s\n\n", " ", " ", trabajadorXPieza.getPagoXPieza(),trabajadorXPieza.getCantidad(), empleado.sueldo());
+                }
+                default -> {
+                }
+            }
+            contador++;
+        }
+    }
+
     public void modificarDatos() {
         int dia = 1, mes = 1, anio = 2000, numeroExterior = 1, numeroInterior = 1, codigoPostal = 1, opcion = 1;
         boolean encontrado = false;
@@ -311,15 +436,15 @@ public class GestionEmpleados {
                             case 1 -> {
                                 System.out.print("Ingrese el o los nombres: ");
                                 empleado.setNombres(sc.nextLine());
-                        }
+                            }
                             case 2 -> {
                                 System.out.print("Ingrese el apellido Paterno: ");
                                 empleado.setApellidoPaterno(sc.nextLine());
-                        }
+                            }
                             case 3 -> {
                                 System.out.print("Ingrese el apellido Materno: ");
                                 empleado.setApellidoMaterno(sc.nextLine());
-                        }
+                            }
                             case 4 -> {
                                 do {
                                     try {
@@ -349,7 +474,7 @@ public class GestionEmpleados {
                                     }
                                 } while (true);
                                 empleado.setFechaNacimiento(new Fecha(dia, mes, anio));
-                        }
+                            }
                             case 5 -> {
                                 System.out.print("Ingrese la calle: ");
                                 String calle = sc.nextLine();
@@ -387,89 +512,91 @@ public class GestionEmpleados {
                                 System.out.print("Ingrese el estado: ");
                                 String estado = sc.nextLine();
                                 empleado.setDomicilio(new Direccion(calle, numeroExterior, numeroInterior, colonia, codigoPostal, ciudad, estado));
-                        }
+                            }
                             case 6 -> {
                                 System.out.print("Ingrese el RFC del empleado: ");
                                 empleado.setRfc(sc.nextLine());
-                        }
+                            }
                             case 7 -> {
                                 System.out.print("Ingrese el departamento asignado: ");
                                 empleado.setDepartamento(sc.nextLine());
-                        }
-                            default -> System.err.println("OPCION NO VALIDA");
+                            }
+                            default ->
+                                System.err.println("OPCION NO VALIDA");
                         }
                     }
                     case 2 -> {
-                    switch (empleado) {
-                        case Jefe jefe -> {
-                            System.out.println("SUELDO SEMANAL: $" + jefe.getSueldoSemanal() + "\n");
-                            do {
-                                try {
-                                    System.out.print("Ingrese el sueldo semanal: $");
-                                    jefe.setSueldoSemanal(Double.parseDouble(sc.nextLine()));
-                                    break;
-                                } catch (NumberFormatException e) {
-                                    System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
-                                }
-                            } while (true);
-                        }
-                        case TrabajadorXComision trabajadorXComision -> {
-                            System.out.println("SALARIO BASE: $" + trabajadorXComision.getSalarioBase()
-                                    + "\nCOMISION: " + trabajadorXComision.getComision() + "\n");
-                            do {
-                                try {
-                                    System.out.print("Introduzca el salario base del empleado: $");
-                                    trabajadorXComision.setSalarioBase(Double.parseDouble(sc.nextLine()));
-                                    break;
-                                } catch (NumberFormatException e) {
-                                    System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
-                                }
-                            } while (true);
-                            double comision;
-                            do {
-                                try {
-                                    System.out.print("Ingrese la comision del empleado (Entre 0 a 1): ");
-                                    comision = Double.parseDouble(sc.nextLine());
-                                    if (comision > 1 || comision < 0) {
-                                        System.out.println("\nCantidad invalida. Introduzca un valor entre (0, 1)");
-                                    } else {
-                                        trabajadorXComision.setComision(comision);
+                        switch (empleado) {
+                            case Jefe jefe -> {
+                                System.out.println("SUELDO SEMANAL: $" + jefe.getSueldoSemanal() + "\n");
+                                do {
+                                    try {
+                                        System.out.print("Ingrese el sueldo semanal: $");
+                                        jefe.setSueldoSemanal(Double.parseDouble(sc.nextLine()));
                                         break;
+                                    } catch (NumberFormatException e) {
+                                        System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
                                     }
-                                } catch (NumberFormatException e) {
-                                    System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
-                                }
-                            } while (true);
-                        }
-                        case TrabajadorXHora trabajadorXHora -> {
-                            System.out.println("PAGO POR Hora: $" + trabajadorXHora.getPagoXHora() + "\n");
-                            do {
-                                try {
-                                    System.out.print("Ingrese el pago por hora: $");
-                                    trabajadorXHora.setPagoXHora(Double.parseDouble(sc.nextLine()));
-                                    break;
-                                } catch (NumberFormatException e) {
-                                    System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
-                                }
-                            } while (true);
-                        }
-                        case TrabajadorXPieza trabajadorXPieza -> {
-                            System.out.println("PAGO POR PIEZA: $" + trabajadorXPieza.getPagoXPieza() + "\n");
-                            do {
-                                try {
-                                    System.out.print("Ingrese el pago por pieza: $");
-                                    trabajadorXPieza.setPagoXPieza(Double.parseDouble(sc.nextLine()));
-                                    break;
-                                } catch (NumberFormatException e) {
-                                    System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
-                                }
-                            } while (true);
-                        }
-                        default -> {
+                                } while (true);
+                            }
+                            case TrabajadorXComision trabajadorXComision -> {
+                                System.out.println("SALARIO BASE: $" + trabajadorXComision.getSalarioBase()
+                                        + "\nCOMISION: " + trabajadorXComision.getComision() + "\n");
+                                do {
+                                    try {
+                                        System.out.print("Introduzca el salario base del empleado: $");
+                                        trabajadorXComision.setSalarioBase(Double.parseDouble(sc.nextLine()));
+                                        break;
+                                    } catch (NumberFormatException e) {
+                                        System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+                                    }
+                                } while (true);
+                                double comision;
+                                do {
+                                    try {
+                                        System.out.print("Ingrese la comision del empleado (Entre 0 a 1): ");
+                                        comision = Double.parseDouble(sc.nextLine());
+                                        if (comision > 1 || comision < 0) {
+                                            System.out.println("\nCantidad invalida. Introduzca un valor entre (0, 1)");
+                                        } else {
+                                            trabajadorXComision.setComision(comision);
+                                            break;
+                                        }
+                                    } catch (NumberFormatException e) {
+                                        System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+                                    }
+                                } while (true);
+                            }
+                            case TrabajadorXHora trabajadorXHora -> {
+                                System.out.println("PAGO POR Hora: $" + trabajadorXHora.getPagoXHora() + "\n");
+                                do {
+                                    try {
+                                        System.out.print("Ingrese el pago por hora: $");
+                                        trabajadorXHora.setPagoXHora(Double.parseDouble(sc.nextLine()));
+                                        break;
+                                    } catch (NumberFormatException e) {
+                                        System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+                                    }
+                                } while (true);
+                            }
+                            case TrabajadorXPieza trabajadorXPieza -> {
+                                System.out.println("PAGO POR PIEZA: $" + trabajadorXPieza.getPagoXPieza() + "\n");
+                                do {
+                                    try {
+                                        System.out.print("Ingrese el pago por pieza: $");
+                                        trabajadorXPieza.setPagoXPieza(Double.parseDouble(sc.nextLine()));
+                                        break;
+                                    } catch (NumberFormatException e) {
+                                        System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+                                    }
+                                } while (true);
+                            }
+                            default -> {
+                            }
                         }
                     }
-                    }
-                    default -> System.err.println("OPCION NO VALIDA");
+                    default ->
+                        System.err.println("OPCION NO VALIDA");
                 }
             }
         }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,13 @@
 
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args) {
         GestionEmpleados gerente = new GestionEmpleados();
-        
+        int opcion = 1;
+        Scanner sc = new Scanner(System.in);
+
         Fecha fechaNacimiento = new Fecha(15, 3, 1985);
         Direccion direccion = new Direccion("Av. Principal", 123, 456, "Centro", 12345, "Ciudad", "Estado");
         Jefe jefe = new Jefe("Carlos", "Pérez", "Gómez", fechaNacimiento, direccion, "CAR123456", "Dirección General", 5000);
@@ -16,8 +20,37 @@ public class main {
         gerente.setEmpleados(trabajadorXPieza);
         gerente.setEmpleados(trabajadorXHora);
         
-        gerente.modificarDatos();
-        gerente.consultarDatos();
-        
+        do {
+            System.out.print("""
+                           SELECCIONE ALGUNA OPCION 
+                           0) Salir
+                           1) Contratar empleado
+                           2) Despedir empleado
+                           3) Cargar datos de nomina
+                           4) Generar nomina
+                           5) Consultar datos de empleado
+                           6) Modificar datos
+                           OPCION:""");
+            do {
+                try {
+                    opcion = Integer.parseInt(sc.nextLine());
+                    break;
+                } catch (NumberFormatException e) {
+                    System.err.println("\nEntrada invalida. Vuelva a intentar...\n");
+                }
+            } while (true);
+            System.out.println(" ");
+            switch (opcion) {
+                case 0 -> System.out.println("FIN DEL PROGRAMA");
+                case 1 -> gerente.contratarEmpleado();
+                case 2 -> gerente.despedirEmpleado();
+                case 3 -> gerente.cargaDatosDomina();
+                case 4 -> gerente.generarNomina();
+                case 5 -> gerente.consultarDatos();
+                case 6 -> gerente.modificarDatos();
+                default -> System.out.println("Opcion no disponible");
+            }
+            System.out.println(" ");
+        } while (opcion != 0);
     }
 }
